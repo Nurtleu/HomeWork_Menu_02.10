1e84ba5c91e1b0a4b2cc3fa227e9ef209d01e956
Nurtleu <nur71701@gmail.com>
2017-10-05 15:16:36 +0600

    HomeWork10

---
+++ WinFormsHW10/WinFormsHW10/.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WinFormsHW10
+{
+    partial class TextEditor
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuStripEditor = new System.Windows.Forms.MenuStrip();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CreateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CreateProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenProjectToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddExistingProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.EditToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CutOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CopyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.InsertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.DeleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SelectAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileInfoTextBox = new System.Windows.Forms.TextBox();
+            this.MenuStripEditor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuStripEditor
+            // 
+            this.MenuStripEditor.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem,
+            this.EditToolStripMenuItem});
+            this.MenuStripEditor.Location = new System.Drawing.Point(0, 0);
+            this.MenuStripEditor.Name = "MenuStripEditor";
+            this.MenuStripEditor.Size = new System.Drawing.Size(740, 24);
+            this.MenuStripEditor.TabIndex = 0;
+            this.MenuStripEditor.Text = "menuStrip1";
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CreateToolStripMenuItem,
+            this.OpenFileToolStripMenuItem,
+            this.AddToolStripMenuItem,
+            this.SaveToolStripMenuItem,
+            this.ExitToolStripMenuItem});
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.FileToolStripMenuItem.Text = "Файл";
+            // 
+            // CreateToolStripMenuItem
+            // 
+            this.CreateToolStripMenuItem.Checked = true;
+            this.CreateToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CreateToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CreateProjectToolStripMenuItem});
+            this.CreateToolStripMenuItem.Name = "CreateToolStripMenuItem";
+            this.CreateToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.CreateToolStripMenuItem.Text = "Создать";
+            // 
+            // CreateProjectToolStripMenuItem
+            // 
+            this.CreateProjectToolStripMenuItem.Name = "CreateProjectToolStripMenuItem";
+            this.CreateProjectToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.N)));
+            this.CreateProjectToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.CreateProjectToolStripMenuItem.Text = "Проект";
+            // 
+            // OpenFileToolStripMenuItem
+            // 
+            this.OpenFileToolStripMenuItem.Checked = true;
+            this.OpenFileToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.OpenFileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenProjectToolStrip});
+            this.OpenFileToolStripMenuItem.Name = "OpenFileToolStripMenuItem";
+            this.OpenFileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.OpenFileToolStripMenuItem.Text = "Открыть";
+            // 
+            // OpenProjectToolStrip
+            // 
+            this.OpenProjectToolStrip.Name = "OpenProjectToolStrip";
+            this.OpenProjectToolStrip.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.O)));
+            this.OpenProjectToolStrip.Size = new System.Drawing.Size(189, 22);
+            this.OpenProjectToolStrip.Text = "Проект";
+            this.OpenProjectToolStrip.Click += new System.EventHandler(this.OpenProjectToolStrip_Click);
+            // 
+            // AddToolStripMenuItem
+            // 
+            this.AddToolStripMenuItem.Checked = true;
+            this.AddToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.AddToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddExistingProjectToolStripMenuItem});
+            this.AddToolStripMenuItem.Name = "AddToolStripMenuItem";
+            this.AddToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.AddToolStripMenuItem.Text = "Добавить";
+            // 
+            // AddExistingProjectToolStripMenuItem
+            // 
+            this.AddExistingProjectToolStripMenuItem.Name = "AddExistingProjectToolStripMenuItem";
+            this.AddExistingProjectToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.AddExistingProjectToolStripMenuItem.Text = "Существующий проект";
+            this.AddExistingProjectToolStripMenuItem.Click += new System.EventHandler(this.ExistingProjectToolStrip_Click);
+            // 
+            // SaveToolStripMenuItem
+            // 
+            this.SaveToolStripMenuItem.Checked = true;
+            this.SaveToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.SaveToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SaveAsToolStripMenuItem});
+            this.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem";
+            this.SaveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.SaveToolStripMenuItem.Text = "Сохранить";
+            // 
+            // SaveAsToolStripMenuItem
+            // 
+            this.SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem";
+            this.SaveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.SaveAsToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.SaveAsToolStripMenuItem.Text = "Сохранить как";
+            this.SaveAsToolStripMenuItem.Click += new System.EventHandler(this.SaveAsToolStrip_Click);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Checked = true;
+            this.ExitToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ExitToolStripMenuItem.Text = "Выход";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStrip_Click);
+            // 
+            // EditToolStripMenuItem
+            // 
+            this.EditToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CutOutToolStripMenuItem,
+            this.CopyToolStripMenuItem,
+            this.InsertToolStripMenuItem,
+            this.DeleteToolStripMenuItem,
+            this.SelectAllToolStripMenuItem});
+            this.EditToolStripMenuItem.Name = "EditToolStripMenuItem";
+            this.EditToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.EditToolStripMenuItem.Text = "Правка";
+            // 
+            // CutOutToolStripMenuItem
+            // 
+            this.CutOutToolStripMenuItem.Checked = true;
+            this.CutOutToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CutOutToolStripMenuItem.Name = "CutOutToolStripMenuItem";
+            this.CutOutToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.CutOutToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.CutOutToolStripMenuItem.Text = "Вырезать";
+            this.CutOutToolStripMenuItem.Click += new System.EventHandler(this.CutOutToolStripMenuItem_Click);
+            // 
+            // CopyToolStripMenuItem
+            // 
+            this.CopyToolStripMenuItem.Checked = true;
+            this.CopyToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem";
+            this.CopyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.CopyToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.CopyToolStripMenuItem.Text = "Копировать";
+            this.CopyToolStripMenuItem.Click += new System.EventHandler(this.CopyToolStripMenuItem_Click);
+            // 
+            // InsertToolStripMenuItem
+            // 
+            this.InsertToolStripMenuItem.Checked = true;
+            this.InsertToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.InsertToolStripMenuItem.Name = "InsertToolStripMenuItem";
+            this.InsertToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.InsertToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.InsertToolStripMenuItem.Text = "Вставить";
+            this.InsertToolStripMenuItem.Click += new System.EventHandler(this.InsertToolStripMenuItem_Click);
+            // 
+            // DeleteToolStripMenuItem
+            // 
+            this.DeleteToolStripMenuItem.Checked = true;
+            this.DeleteToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem";
+            this.DeleteToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
+            this.DeleteToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.DeleteToolStripMenuItem.Text = "Удалить";
+            this.DeleteToolStripMenuItem.Click += new System.EventHandler(this.DeleteToolStripMenuItem_Click);
+            // 
+            // SelectAllToolStripMenuItem
+            // 
+            this.SelectAllToolStripMenuItem.Checked = true;
+            this.SelectAllToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.SelectAllToolStripMenuItem.Name = "SelectAllToolStripMenuItem";
+            this.SelectAllToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.SelectAllToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.SelectAllToolStripMenuItem.Text = "Выделить все";
+            this.SelectAllToolStripMenuItem.Click += new System.EventHandler(this.SelectAllToolStripMenuItem_Click);
+            // 
+            // FileInfoTextBox
+            // 
+            this.FileInfoTextBox.Location = new System.Drawing.Point(30, 48);
+            this.FileInfoTextBox.Multiline = true;
+            this.FileInfoTextBox.Name = "FileInfoTextBox";
+            this.FileInfoTextBox.Size = new System.Drawing.Size(685, 440);
+            this.FileInfoTextBox.TabIndex = 1;
+            // 
+            // TextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 567);
+            this.Controls.Add(this.FileInfoTextBox);
+            this.Controls.Add(this.MenuStripEditor);
+            this.MainMenuStrip = this.MenuStripEditor;
+            this.Name = "TextEditor";
+            this.Text = "Text editor";
+            this.MenuStripEditor.ResumeLayout(false);
+            this.MenuStripEditor.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuStripEditor;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AddToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SaveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem EditToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CopyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem InsertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem DeleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SelectAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CreateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CreateProjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenProjectToolStrip;
+        private System.Windows.Forms.ToolStripMenuItem AddExistingProjectToolStripMenuItem;
+        private System.Windows.Forms.TextBox FileInfoTextBox;
+        private System.Windows.Forms.ToolStripMenuItem CutOutToolStripMenuItem;
+    }
+}
+
---
+++ WinFormsHW10/WinFormsHW10/TextEditor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsHW10
+{
+    public partial class TextEditor : Form
+    {
+        string file;
+        public TextEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitToolStrip_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void SaveAsToolStrip_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog file = new SaveFileDialog();
+            file.ShowDialog();
+        }
+
+        private void ExistingProjectToolStrip_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.ShowDialog();
+        }
+
+        private void OpenProjectToolStrip_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog project = new OpenFileDialog();
+            project.ShowDialog();
+        }
+
+        private void CutOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.FileInfoTextBox.Cut();
+        }
+
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem copyFile = new ToolStripMenuItem();
+            copyFile.Click += CopyToolStripMenuItem_Click;
+            file = FileInfoTextBox.SelectedText;
+        }
+
+        private void InsertToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem pasteFile = new ToolStripMenuItem();
+            pasteFile.Click += InsertToolStripMenuItem_Click;
+            FileInfoTextBox.Paste(file);
+        }
+
+        private void DeleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.FileInfoTextBox.Clear();
+        }
+
+        private void SelectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.FileInfoTextBox.SelectAll();
+        }
+    }
+}
